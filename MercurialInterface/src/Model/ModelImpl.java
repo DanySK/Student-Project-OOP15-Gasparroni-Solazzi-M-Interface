231169ca79c0e6298b0a7f0c78e6ec50179fd1d3
filo2 <filo2@localhost>
2016-09-26 00:44:34 +0200

    Aggiunta Progetto, Relazione e File .jar

---
+++ MercurialInterface/src/Model/ModelImpl.java
@@ -0,0 +1,125 @@
+/**
+ * Classe utilizzata per verificare l'esistenza di mercurial e gestire i vari comandi
+ *
+ * @author  Silvia Gasparroni
+ *
+ */
+package Model;
+
+import java.io.PrintStream;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+
+public class ModelImpl implements Model {
+	
+	private static String cmd;
+	private static Process p;
+
+	private String dir;
+	private String repo;
+	static boolean va=true; 
+	private static boolean bool;
+	
+	
+	private static String cmdArray(){
+		String cmd;
+		if (!System.getProperty("os.name").contains("Windows")){
+			cmd="/usr/bin/ start";
+			JOptionPane.showMessageDialog(null, "utilizzare su windows" , "Attenzione", JOptionPane.WARNING_MESSAGE);
+		}else{
+			cmd="cmd";
+		}
+		return cmd;
+	}
+	
+	@Override
+	public void setCmd(){
+		String str=cmdArray();
+		cmd=str;
+		CommandModelProject.setCommand(str);
+		ChoiseRepository.setCommand(str);
+		CommandLog.setCommand(str);
+	}
+	
+	@Override
+	 public void doCopy(String elem) {
+		 if (!System.getProperty("os.name").contains("Windows")){
+				CommandModelProject.print("cp",repo);
+			}else{
+				CommandModelProject.print("copy "+elem +" "+repo," ");
+			}
+	}
+	
+	@Override 
+	public void setRepository(String repo){
+		this.repo = repo;
+		ChoiseRepository.setDirectory(repo);
+    	ChoiseRepository.CreateRepository();
+	}
+	
+	@Override 
+	public boolean check_Existence() {
+    setCmd();
+		try {
+			p = Runtime.getRuntime().exec(cmd);
+			int c=1;
+			new Thread(new Thrmod(p.getErrorStream(), System.err,c)).start();
+			new Thread(new Thrmod(p.getInputStream(), System.out)).start();
+			PrintStream stdin = new PrintStream(p.getOutputStream());
+			stdin.println("hg");
+		    stdin.flush();
+		    stdin.close();
+		    p.waitFor();
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			
+		} finally{
+		   //String str=Thrmod.ReturnString.getList().toString();
+			 if(!Thrmod.ReturnString.state()){
+				 bool = true;
+			   //System.out.println("errore: "+str);
+			   Thrmod.ReturnString.reset();
+			 }	else{
+				 bool=false;
+			 }
+		}
+		return bool;
+	}
+	
+	@Override 
+	public void doCommit(String commento)  {
+		CommandModelProject.print("hg commit -m" + " " + commento , this.repo);
+	}
+	
+	@Override 
+	public List<String> doLog()  {
+		return CommandLog.print("hg log", this.repo);
+	}
+	
+	@Override 
+	public void doAdd(String dir)  {
+		CommandModelProject.print("hg add "+ dir, this.repo);
+		
+	}
+	
+	@Override 
+	public void clone_Repository(String url) {
+		CommandModelProject.print("hg clone "+url, this.dir);
+		
+	}
+	
+	@Override 
+	public void doUser(String username){
+		JOptionPane.showMessageDialog(null, "inserire username="+username+"<nome@email.it>", "login", JOptionPane.INFORMATION_MESSAGE);
+		CommandModelProject.print("hg config --edit","");
+	}
+	
+	@Override
+	public void push(String url) {
+		CommandModelProject.print("hg push " + url , this.repo);
+	}
+	
+}
---
+++ MercurialInterface/src/View/Push_View.java
@@ -0,0 +1,97 @@
+package View;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import Control.Control;
+
+/**
+ * Classe utilizzata per inserire l'URL di una repository
+ * in cui andare ad inserire gli elementi della repository locale
+ *
+ * @author Filippo Solazzi
+ *
+ */
+public class Push_View extends JFrame{
+
+	private static final long serialVersionUID = 5546526749878417013L;
+	private JTextField url = new JTextField(25);
+	private JLabel label = new JLabel();
+	private JButton ok = new JButton("OK");
+	private JButton exit = new JButton("Annulla");
+	private URL sito;
+	
+	/**
+	* Costruttore che inizializza la propria View
+	* con i rispettivi eventi
+	*
+	* @param ctr
+	*          Control utilizzato per l'intercetto degli eventi
+	*/
+	public Push_View(Control ctr){
+		this.setTitle("Choose the repository to be cloned");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLayout(new BorderLayout());
+		this.setSize(600,120);
+		this.setLocationRelativeTo(null);
+		
+		label.setText("Inserire la repository su cui effettuare il push");
+		JPanel panel = new JPanel();
+		JPanel bpanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		
+		ok.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				
+				//Controllo dell'esistenza e della sintassi dell'URL
+				try {
+					if(url.getText().indexOf(".") != -1 && url.getText().indexOf("\\") == -1){
+						sito = new URL(url.getText());
+						HttpURLConnection huc =  ( HttpURLConnection )  sito.openConnection (); 
+						if(huc.getResponseCode() == 200 || huc.getResponseCode() == 202 || huc.getResponseCode() == 302){
+							ctr.pushed(url.getText());
+							dispose();
+						}
+						else{
+							JOptionPane.showMessageDialog(null, "URL non trovato", "Errore", JOptionPane.ERROR_MESSAGE);
+						}
+					}
+					else{
+						JOptionPane.showMessageDialog(null, "Sintassi URL Errata", "Errore", JOptionPane.ERROR_MESSAGE);
+					}
+				}
+				catch (MalformedURLException e1) {
+					JOptionPane.showMessageDialog(null, "Sintassi URL Errata \n(Aggiungere http://)", "Errore", JOptionPane.ERROR_MESSAGE);
+				}
+				catch(IOException e1){
+					JOptionPane.showMessageDialog(null, "Errore Generico", "Errore", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+		
+		exit.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				dispose();
+			}
+		});
+		
+		panel.add(label);
+		panel.add(url);
+		bpanel.add(ok);
+		bpanel.add(exit);
+		this.add(panel, BorderLayout.NORTH);
+		this.add(bpanel, BorderLayout.SOUTH);
+	}
+}
